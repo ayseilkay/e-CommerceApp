d7a58658dcd728502ab8b6fcead15f4a14651306
ayseilkay <aayseilkay@gmail.com>
2022-01-16 23:26:48 +0300

    Sign Up - formik && yup

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,20 @@
 import axios from 'axios'
+//getAll Product
 export const fetchproductList = async({ pageParam = 1 }) =>{
-    const {data} = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}?page=${pageParam}`);
+    const {data} = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/products?page=${pageParam}`);
     return data;
 }
 
+//get oneProduct
 export const fetchOneProduct = async(id)=>{
-    console.log(`https://fakestoreapi.com/products/${id}`)
-    const {data} = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}/${id}`);
+    // console.log(`https://fakestoreapi.com/products/${id}`)
+    const {data} = await axios.get(`${process.env.REACT_APP_BASE_ENDPOINT}products/${id}`);
     return data;
+}
+// post -> add a User
+
+
+export const fetchRegister = async(input)=>{
+    const {data} = await axios.post(`https://reqres.in/api/users`,{input});
+    return data; 
 }
--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -1,9 +1,77 @@
+import { Flex,Box,Heading,FormControl ,FormLabel,Input, Button, Alert} from '@chakra-ui/react'
 import React from 'react'
+import { Formik, useFormik } from 'formik';
+import validationSchema from './validations';
+import { fetchRegister } from '../../../api';
+import axios from 'axios';
+
 
 function Signup() {
+
+   
+    const {handleChange,handleSubmit,handleBlur,values,errors,touched} = useFormik({
+        initialValues:{
+          email: '',
+          password:'',
+          passwordConfirm:''
+        },
+        onSubmit: async(values,bag) => {
+          try{
+            const registerResponse = await  fetchRegister({email:values.email,password:values.password});
+          }catch(e){
+             bag.setErrors({general:e.response.data.message})
+          }
+        },
+        
+        validationSchema,
+      });
+
+      
     return (
         <div>
-           <h1> signup</h1>
+           <Flex align="center" width="full" justifyContent="center">
+                <Box pt="10">
+                    <Box textAlign="center">
+                        <Heading>
+                            Sign Up
+                        </Heading>
+                    </Box>
+                    <Box my={"5"}>
+                        {
+                            errors.general && (
+                                <Alert status='error'>{errors.general}</Alert>
+                            )
+                        }
+                    </Box>
+                    <Box my={5} textAlign="left">
+                        <form onSubmit={handleSubmit}>
+                            <FormControl mt={"4"}>
+                                <FormLabel>Email</FormLabel>
+                                <Input name="email" type={"email"} value={values.email} onChange={handleChange} onBlur={handleBlur} isInvalid={ errors.email && touched.email}/>
+                                {
+                                errors.email && touched.email && (<div className='error'> {errors.email}</div>
+                                )}
+                            </FormControl>
+                            <FormControl mt={"4"}>
+                                <FormLabel>Password</FormLabel>
+                                <Input name="password" type="password" value={values.password} onChange={handleChange} onBlur={handleBlur} isInvalid={errors.password && touched.password}/>
+                                {
+                                errors.password && touched.password && (<div className='error'> {errors.password}</div>
+                                )}
+                            </FormControl>
+                            <FormControl mt={"4"}>
+                                <FormLabel>Password Confirm</FormLabel>
+                                <Input name="passwordConfirm" type="password" value={values.passwordConfirm} onChange={handleChange} onBlur={handleBlur} isInvalid={errors.passwordConfirm && touched.passwordConfirm}/>
+                                 {
+                                errors.passwordConfirm && touched.passwordConfirm && (<div className='error'> {errors.passwordConfirm}</div>
+                                )}
+                            </FormControl>
+
+                            <Button mt="4" width={"full"} type='submit' colorScheme={"green"}>Sign Up</Button>
+                        </form>
+                    </Box>
+                </Box>
+           </Flex>
         </div>
     )
 }
